a027073901b31d9f7cd9b1a609912e4c00981d4b
Atinos31 <atinosandra2014@gmail.com>
2021-04-28 11:05:36 +0200

    emailjs implemented

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -16,3 +16,20 @@
         });
     }, false);
 })();
+
+function sendMail(contactForm) {
+    emailjs.send("gmail", "template_04ew0co", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "feedback_summary": contactForm.feedbacksummary.value
+        })
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+            },
+            function(error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block from loading a new page
+}
