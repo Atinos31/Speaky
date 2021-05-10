a7ac7a46a79a1266c8a465bebccd1234bf661735
Atinos31 <atinosandra2014@gmail.com>
2021-05-10 11:19:53 +0200

    success box on button click implemented

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -7,13 +7,40 @@ function sendForm(contactForm) {
         .then(
             function(response) {
                 console.log('SUCCESS!', response.status, response.text)
-                alert('Your message has been sent!');
+
             },
             function(error) {
                 console.log('FAILED', error);
-                alert('ooops something gone wrong!')
+
             }
         );
     return false; // To block from loading a new page
 
+}
+
+//modal after pressing submit button on feedback form
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
