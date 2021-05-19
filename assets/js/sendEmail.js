b0daf798a26ffc35f04103247703e5435ec174be
Atinos31 <atinosandra2014@gmail.com>
2021-05-19 18:11:52 +0200

    finall bugs fixed

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
-    // code from https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_event_submit_prevent customised to fit needs
     $("#form").submit(function(event) {
-        event.preventDefault();
+        event.preventDefault(); // prevents default form submission 
         const serviceID = "speaky";
         const templateID = "template_imyx42d";
         emailjs.sendForm(serviceID, templateID, this)
             .then(() => {
-                //Display modal success
+                //Display modal -confirmation message
                 $('#myModal').removeClass("hide");
                 // alert(' Your message has been sent Successfully.')
                 $("#form")[0].reset();
